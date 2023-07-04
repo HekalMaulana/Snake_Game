90f591a94a8ce91f0ce38de044975df3b91dfa86
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2023-07-04 22:12:22 +0800

    Snake Game 4
    -Making random position of food
    -Create a score board if snake eat the food

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 from turtle import Screen
-
+from food import Food
 from snake import Snake
+from scoreboard import Score
 
 screen = Screen()
 screen.tracer(0)
@@ -10,7 +11,9 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Welcome to my SNAKE GAME")
 
-snake = Snake(shape="square", color="white")
+snake = Snake()
+food = Food()
+score = Score()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,9 +25,13 @@ is_game_on = True
 
 while is_game_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.15)
 
     snake.move()
+
+    if snake.head.distance(food) < 17:
+        food.refresh()
+        score.add_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         is_game_on = False
