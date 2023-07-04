ac5db5fda023033114d4127db89ba5cf3af69f7a
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2023-07-04 22:22:11 +0800

    scoreboard.py 1
    -Creating a score board in window
    -Make a position of score board in center top of window
    -Make a score increase 1 if snake eat the food

---
+++ scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+ALIGMENT = 'center'
+FONT = ("Courier", 22, "normal")
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(x=0, y=270)
+        self.update_score()
+
+    def update_score(self):
+        self.write(arg=f"Score: {self.score}", align= ALIGMENT, font= FONT)
+
+    def add_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
