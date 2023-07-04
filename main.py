c472fe3f85b3be92d64e1a833dfdbd1d00bd2671
HekalMaulana <116002257+HekalMaulana@users.noreply.github.com>
2023-07-04 22:57:26 +0800

    Snake Game 5
    -Create a new tail if snake eat the food
    -Making condition when head of snake hit his tail then the game is over

--- main.py
+++ main.py
@@ -31,11 +31,18 @@ while is_game_on:
 
     if snake.head.distance(food) < 17:
         food.refresh()
+        snake.extend()
         score.add_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         is_game_on = False
-        screen.bye()
-        print("You hit the wall")
+        score.game_over()
 
-# screen.exitonclick()
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 1:
+            is_game_on = False
+            score.game_over()
+
+screen.exitonclick()
